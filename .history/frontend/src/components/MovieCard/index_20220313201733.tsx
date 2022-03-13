f7371330b37de25f87bf375a60841a1fe7bfbecd
Luiz César Pacheco dos Santos <lcpsantos@gmail.com>
2022-03-13 20:23:22 -0300

    Navigation buttons

--- .history/frontend/src/components/MovieCard/index_20220313201733.tsx
+++ .history/frontend/src/components/MovieCard/index_20220313201733.tsx
@@ -22,10 +22,10 @@ function MovieCard() {
         <h3>{movie.title}</h3>
         <MovieScore />
 
-        <Link to={`/form/${movie.id}`}>
+        <Link to="/form/1">
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
-
+        
       </div>
     </div>
   );
